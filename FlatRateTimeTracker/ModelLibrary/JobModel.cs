b54fca08d686bdb3a0f608620dc68bcaae153fc8
Daxxn <46167982+Daxxn@users.noreply.github.com>
2020-05-15 13:03:13 -0700

    Initial commit
    Master commit for boilerplate code and initial project setup.

---
+++ FlatRateTimeTracker/ModelLibrary/JobModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    public class JobModel
+    {
+        #region - Fields
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan JobTime { get; set; }
+        #endregion
+
+        #region - Constructors
+        public JobModel() { }
+        #endregion
+
+        #region - Methods
+
+        #endregion
+
+        #region - Properties
+
+        #endregion
+    }
+}
