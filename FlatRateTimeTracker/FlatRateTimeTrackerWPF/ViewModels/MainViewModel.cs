da45744dff45d8f15ea57ca8779e6e65398507ff
Daxxn <46167982+Daxxn@users.noreply.github.com>
2020-05-16 18:40:18 -0700

    Fix commit fuckup

---
+++ FlatRateTimeTracker/FlatRateTimeTrackerWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ModelLibrary;
+
+namespace FlatRateTimeTrackerWPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region - Fields
+        public JobController JobController { get; set; }
+        public JobType SelectedJobType { get; set; }
+        #endregion
+
+        #region - Constructors
+        public MainViewModel()
+        {
+            JobController = new JobController()
+            {
+                AllJobs = new List<JobModel>()
+                {
+                    new JobModel()
+                    {
+                        StartTime = DateTime.Now,
+                        EndTime = DateTime.Now.AddHours(1),
+                        Type = JobType.MinorOilChange,
+                    }
+                }
+            };
+        }
+
+        public void StartJobClickEvent(object sender, RoutedEventArgs e)
+        {
+            JobController.StartJob(SelectedJobType);
+        }
+
+        public void StartJobKeyEvent(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                JobController.StartJob(SelectedJobType);
+            }
+        }
+        #endregion
+
+        #region - Methods
+
+        #endregion
+
+        #region - Properties
+
+        #endregion
+    }
+}
